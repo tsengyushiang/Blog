fc4f4a7e30c308375bff1b11c4ee4cedace20567
tsengyushiang <tsengyushiang@gmail.com>
2022-05-04 22:22:34 +0800

    Initial commit

---
+++ next.config.js
@@ -0,0 +1,26 @@
+/** @type {import('next').NextConfig} */
+
+let nextConfig = {
+  reactStrictMode: true,
+  env: {
+    TIME: new Date(+new Date() + 8 * 3600 * 1000).toUTCString(),
+  },
+}
+
+if (process.env.NODE_ENV == 'production') {
+  const githubPagesConfig = (github_repo_name) => {
+    return {
+      assetPrefix: `/${github_repo_name}`,
+      basePath: `/${github_repo_name}`,
+      images: {
+        loader: "akamai",
+        path: `/${github_repo_name}`
+      }
+    }
+  }
+  nextConfig = {
+    ...nextConfig,
+    ...githubPagesConfig(`${process.env.NEXT_PUBLIC_REPO}`.split('/')[1])
+  }
+}
+module.exports = nextConfig
